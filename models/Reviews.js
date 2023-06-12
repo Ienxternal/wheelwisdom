b61dbc8cb4288ea037affc96b58022420be8a330
rikilega <rosedoland33@yahoo.com>
2023-06-12 16:54:23 -0700

    added model files- reviews, vehicles, and index

---
+++ models/Reviews.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+Review.init(
+    {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    review_content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    following_user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+        },
+    },
+    reviewed_vehicle_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'vehicle',
+            key: 'id',
+        },
+    },
+    },
+    {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'review',
+    }
+);
+
+module.exports = Review;
+
