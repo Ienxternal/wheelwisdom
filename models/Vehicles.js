3ef25b618a3d1b6b35e963f59acfb6a4a15482b8
rikilega <rosedoland33@yahoo.com>
2023-06-12 18:17:28 -0700

    added reviews.js, vehicles.js

---
+++ models/Vehicles.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Vehicle extends Model {}
+
+Vehicle.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        year: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        make: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        model: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'vehicle',
+    }
+);
+
+module.exports = Vehicle;
+
+module.exports = Gallery;
