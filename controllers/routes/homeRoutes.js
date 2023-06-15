eacf5a1ad284eeadbf408675425673caeab4d48c
laurens <lauren.smi001@gmail.com>
2023-06-15 15:11:44 -0400

    Working on routes

---
+++ controllers/routes/homeRoutes.js
@@ -0,0 +1,96 @@
+const router = require('express').Router();
+const { User } = require('../../../models');
+const withAuth = require('../../../utils/auth');
+
+router.get('/', async (req, res) => {
+  res.render('homepage');
+});
+
+router.get('/product', async (req, res) => {
+  res.render('product');
+});
+router.get('/about', async (req, res) => {
+  res.render('about');
+});
+router.get('/features', async (req, res) => {
+  res.render('features');
+});
+router.get('/pricing', async (req, res) => {
+  res.render('pricing');
+});
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.render('homepage', {
+      users,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//User registration
+
+/*router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});*/
+
+
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+
+module.exports = router;
