2bbd7d5c5bc94843be32736640884ff6f7eb58b1
Ienxternal <alexanderttnguyen@gmail.com>
2023-06-15 17:41:14 -0500

    Functional running node clean

--- controllersOC/dish-routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-// Here is where we provide hardcoded data to render dynamically
-
-//get all dishes
-router.get('/', async (req, res) => {
-  res.render('homepage');
-});
-
-module.exports = router;
