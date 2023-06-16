ae3524c9b701a7be162d94637e5b0e12c35fb24e
Ienxternal <alexanderttnguyen@gmail.com>
2023-06-16 15:23:42 -0500

    Added functional user inputs.

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,39 @@
 
+// const loginFormHandler = async (event) => {
+//   event.preventDefault();
+//       alert('Failed to login');
+// };
+
+
 const loginFormHandler = async (event) => {
   event.preventDefault();
-      alert('Failed to login');
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log in.');
+    }
+  }
 };
+
 
 
 
 
 const signupFormHandler = async (event) => {
   event.preventDefault();
-
-
       alert('Failed to sign up.');
-  
-
 };
-
-
-
 
 document
   .querySelector('.login-form')
