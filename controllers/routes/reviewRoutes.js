a8766f8bf2c10fd94829106473ae5786f5145332
laurens <lauren.smi001@gmail.com>
2023-06-19 15:20:14 -0400

    working on fixing review routes and review submit form

--- controllers/routes/reviewRoutes.js
+++ controllers/routes/reviewRoutes.js
@@ -5,7 +5,7 @@ const Reviews = require('../../models/Reviews');
 router.post('/', async (req, res) => {
     try {
         // Retrieve the user input from the request body
-        const { comment, following_user_id, vehicle_id } = req.body;
+        const { review, following_user_id, vehicle_id } = req.body;
 
         // Sanitize and validate the input (implement your own logic here)
         // For example, you can check if the review is not empty and the following_user_id and vehicle_id exist
@@ -22,7 +22,7 @@ router.post('/', async (req, res) => {
 
         // Make the POST request to your review database using Sequelize
         const newReview = await Reviews.create({
-            comment,
+            review,
             following_user_id,
             vehicle_id
         });
@@ -32,6 +32,48 @@ router.post('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+
+
+
+
+
+
+
+// // CREATE a new review
+// router.post('/', async (req, res) => {
+//     try {
+//         // Retrieve the user input from the request body
+//         const { comment, following_user_id, vehicle_id } = req.body;
+
+//         // Sanitize and validate the input (implement your own logic here)
+//         // For example, you can check if the review is not empty and the following_user_id and vehicle_id exist
+        
+//         // Example validation: Check if the review is not empty
+//         if (!review) {
+//             return res.status(400).json({ error: 'Review cannot be empty' });
+//         }
+
+//         // Example validation: Check if the following_user_id and vehicle_id exist
+//         if (!following_user_id || !vehicle_id) {
+//             return res.status(400).json({ error: 'Invalid following_user_id or vehicle_id' });
+//         }
+
+//         // Make the POST request to your review database using Sequelize
+//         const newReview = await Reviews.create({
+//             comment,
+//             following_user_id,
+//             vehicle_id
+//         });
+
+//         res.status(200).json(newReview);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
+
+
 
 // GET all reviews
 router.get('/', async (req, res) => {
